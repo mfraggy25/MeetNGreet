441dd42435b3220aecc304e8fb6481a25b94d438
mfraggy25 <mfraggy25@users.noreply.github.com>
2020-03-11 16:47:22 -0400

    add back refresh function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -25,3 +25,26 @@ module.exports.getAccessToken = async event => {
     })
   };
 };
+
+module.exports.refreshAccessToken = async event => {
+  const MEETUP_OAUTH_URL =
+    "https://secure.meetup.com/oauth2/access" +
+    "?client_id=lrlpvmncurd803tghsn4kleoqv" +
+    "&client_secret=9bf9lvmd9jlc4q21sr673b0a7m" +
+    "&grant_type=refresh_token" +
+    "&refresh_token=" +
+    event.pathParameters.refresh_token;
+
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token
+    })
+  };
+};
