354cd415f9094a357386f46926e3ec1ea9e37a4e
mfraggy25 <mfraggy25@users.noreply.github.com>
2020-03-28 20:59:00 -0400

    4.2 final corrections

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -9,7 +9,7 @@ module.exports.getAccessToken = async event => {
     "?client_id=lrlpvmncurd803tghsn4kleoqv" +
     "&client_secret=9bf9lvmd9jlc4q21sr673b0a7m" +
     "&grant_type=authorization_code" +
-    "&redirect_uri=https://mfraggy25.github.io/" +
+    "&redirect_uri=https://mfraggy25.github.io/MeetNGreet/" +
     "&code=" +
     event.pathParameters.code;
 
@@ -20,6 +20,7 @@ module.exports.getAccessToken = async event => {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": true
+      //"Access-Control-Allow-Origin": "http://localhost:8080"
     },
     body: JSON.stringify({
       access_token: info.data.access_token,
@@ -35,7 +36,7 @@ module.exports.refreshAccessToken = async event => {
     "&client_secret=9bf9lvmd9jlc4q21sr673b0a7m" +
     "&grant_type=refresh_token" +
     "&refresh_token=" +
-    event.pathParameters.refresh_token;
+    event.pathParameters.code;
 
   const info = await axios.post(MEETUP_OAUTH_URL);
 
@@ -44,6 +45,7 @@ module.exports.refreshAccessToken = async event => {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": true
+      //"Access-Control-Allow-Origin": "http://localhost:8080"
     },
     body: JSON.stringify({
       access_token: info.data.access_token,
@@ -51,3 +53,56 @@ module.exports.refreshAccessToken = async event => {
     })
   };
 };
+
+// "use strict";
+
+// const axios = require("axios");
+
+// module.exports.getAccessToken = async event => {
+//   console.log(event.pathParameters, "test");
+//   const MEETUP_OAUTH_URL =
+//     "https://secure.meetup.com/oauth2/access" +
+//     "?client_id=jr2bsub23bropmv7e9cn3v9mvp" +
+//     "&client_secret=j0jrlqsm7357f31q6n4a9lt2sd" +
+//     "&grant_type=authorization_code" +
+//     "&redirect_uri=https://cilvin.github.io/meetup/" +
+//     "&code=" +
+//     event.pathParameters.code;
+
+//   const info = await axios.post(MEETUP_OAUTH_URL);
+
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       "Access-Control-Allow-Origin": "*"
+//     },
+//     body: JSON.stringify({
+//       access_token: info.data.access_token,
+//       refresh_token: info.data.refresh_token
+//     })
+//   };
+// };
+
+// module.exports.refreshAccessToken = async event => {
+//   const MEETUP_OAUTH_URL =
+//     "https://secure.meetup.com/oauth2/access" +
+//     "?client_id=jr2bsub23bropmv7e9cn3v9mvp" +
+//     "&client_secret=j0jrlqsm7357f31q6n4a9lt2sd" +
+//     "&grant_type=refresh_token" +
+//     "&refresh_token=" +
+//     event.pathParameters.code;
+
+//   const info = await axios.post(MEETUP_OAUTH_URL);
+
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Access-Control-Allow-Origin": "*"
+//     },
+//     body: JSON.stringify({
+//       access_token: info.data.access_token,
+//       refresh_token: info.data.refresh_token
+//     })
+//   };
+// };
