c2a74123fbe30110117ad829666a83de6542e9d4
mfraggy25 <mfraggy25@users.noreply.github.com>
2020-04-02 18:27:08 -0400

    4.4 uncontrolled input error

--- src/EventList.js
+++ src/EventList.js
@@ -1,3 +1,22 @@
+// import React, { Component } from "react";
+// import Event from "./Event";
+
+// class EventList extends Component {
+//   render() {
+//     return (
+//       <ul className="EventList">
+//         {this.props.events.map(event => (
+//           <li key={event.id}>
+//             <Event event={event} />
+//           </li>
+//         ))}
+//       </ul>
+//     );
+//   }
+// }
+
+// export default EventList;
+
 import React, { Component } from "react";
 import Event from "./Event";
 
