c98c823b1c7a6194a5c60957f82f5fb60d8c1754
mfraggy25 <mfraggy25@users.noreply.github.com>
2020-05-03 13:09:47 -0400

    9.9 add pie and dotted charts, style cleanup

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
 import { getEvents } from "./api";
 import { OfflineAlert, WarningAlert } from "./Alert";
+import moment from "moment";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 class App extends Component {
   state = {
@@ -35,6 +45,31 @@ class App extends Component {
         offlineText: "",
       });
     }
+  };
+
+  //finding the event local_dates coming up plugged into getDate
+  countEventsOnADate = (date) => {
+    let count = 0;
+    for (let i = 0; i < this.state.events.length; i += 1) {
+      if (this.state.events[i].local_date === date) {
+        count += 1;
+      }
+    }
+    return count;
+  };
+
+  getData = () => {
+    const next7Days = []; // Create empty array for the next 7 days
+    const currentDate = moment(); // Today
+    // Loop 7 times for next 7 days
+    for (let i = 0; i < 7; i += 1) {
+      currentDate.add(1, "days"); // Add 1 day to current date, currentDate changes
+      const dateString = currentDate.format("YYYY-MM-DD"); // Format the date
+      // Use the countEventsOnADate function to count #events on this date
+      const count = this.countEventsOnADate(dateString);
+      next7Days.push({ date: dateString, number: count }); // Add this date and number to the list
+    }
+    return next7Days;
   };
 
   updateEvents = (lat, lon, page) => {
@@ -70,6 +105,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>MEET N GREET</h1>
+        <h2>Find out what's happening in your city!</h2>
         <OfflineAlert text={this.state.offlineText} />
         <CitySearch updateEvents={this.updateEvents} />
         <NumberOfEvents
@@ -78,6 +115,22 @@ class App extends Component {
           lat={this.state.lat}
           lon={this.state.lon}
         />
+        <ResponsiveContainer height={400}>
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 20,
+              left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="date" name="date" />
+            <YAxis type="number" dataKey="number" name="number of events" />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
         {this.state.noEvent && <WarningAlert text={this.state.infoText} />}
         <EventList events={this.state.events} />
       </div>
--- src/Event.js
+++ src/Event.js
@@ -1,20 +1,38 @@
 import React, { Component } from "react";
+import {
+  PieChart,
+  Pie,
+  Legend,
+  Tooltip,
+  ResponsiveContainer,
+  Line,
+  Cell,
+} from "recharts";
 
 class Event extends Component {
   state = {
     showDetails: false,
+    events: [],
   };
 
-  handleShowDetails = () => {
-    if (this.state.showDetails === false) {
-      this.setState({ showDetails: true });
-    } else {
-      this.setState({ showDetails: false });
-    }
+  handleDetails = () => {
+    this.setState({ showDetails: !this.state.showDetails });
+  };
+
+  getData = () => {
+    const taken = this.props.event.yes_rsvp_count;
+    const limit = this.props.event.rsvp_limit;
+    const open = limit - this.props.event.yes_rsvp_count;
+
+    return [
+      { name: "Spots Taken", value: taken },
+      { name: "Spots Open", value: open },
+    ];
   };
 
   render() {
-    const showDetails = this.state.showDetails;
+    const { event } = this.props;
+    let colors = ["#8884d8", "#82ca9d"];
 
     return (
       <div className="event">
@@ -23,18 +41,64 @@ class Event extends Component {
           <p className="event__Overview--localDate">
             {this.props.event.local_date}
           </p>
-          <button
-            className="details-btn"
-            onClick={() => this.handleShowDetails()}
-          >
-            show details
+          <p className="eventLocationCity">
+            City: {event.venue && event.venue.city}
+          </p>
+          <button className="details-btn" onClick={() => this.handleDetails()}>
+            Show Details
           </button>
         </div>
-        {showDetails && (
-          <div className="event__Details">
-            <p className="event__Details--description">
-              {this.props.event.description}
-            </p>
+        {this.state.showDetails && (
+          <div className="eventDetails">
+            {event.yes_rsvp_count && event.rsvp_limit ? (
+              <ResponsiveContainer height={200}>
+                <PieChart width={200} height={200}>
+                  <Pie
+                    data={this.getData()}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label
+                  >
+                    {this.getData().map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={colors[index]} />
+                    ))}
+                  </Pie>
+
+                  <Tooltip />
+
+                  <Legend verticalAlign="top" height={36}>
+                    <Line
+                      name="Spots Taken"
+                      type="monotone"
+                      dataKey="taken"
+                      stroke="#8884d8"
+                    />
+                    <Line
+                      name="Spots Open"
+                      type="monotone"
+                      dataKey="open"
+                      stroke="#82ca9d"
+                    />
+                  </Legend>
+                </PieChart>
+              </ResponsiveContainer>
+            ) : null}
+
+            <div className="eventVenue">
+              Venue: {event.venue && event.venue.address_1}
+            </div>
+            <div className="eventStatus">Status: {event.status}</div>
+            <div
+              className="eventDescription"
+              dangerouslySetInnerHTML={{ __html: event.description }}
+            ></div>
+            <button className="details-btn" onClick={this.handleDetails}>
+              Hide Details
+            </button>
           </div>
         )}
       </div>
@@ -43,40 +107,3 @@ class Event extends Component {
 }
 
 export default Event;
-// import React, { Component } from "react";
-
-// class Event extends Component {
-//   state = {
-//     event: {},
-//     showDetails: false,
-//   };
-
-//   handleShowDetails = () => {
-//     this.setState({ showDetails: true });
-//   };
-
-//   render() {
-//     const showDetails = this.state.showDetails;
-
-//     return (
-//       <div className="event">
-//         <div className="event__Overview">
-//           <p className="event__Overview--name">{this.state.event.name}</p>
-//           <p className="event__Overview--localDate">
-//             {this.state.event.local_date}
-//           </p>
-//           <button onClick={() => this.handleShowDetails()}>show details</button>
-//         </div>
-//         {showDetails && (
-//           <div className="event__Details">
-//             <p className="event__Details--description">
-//               {this.state.event.description}
-//             </p>
-//           </div>
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
-// export default Event;
